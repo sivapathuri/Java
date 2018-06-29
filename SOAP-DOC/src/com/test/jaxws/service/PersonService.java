e9e03d0c455becad6aa77b49db1fa42d676ecdc1
Siva Reddy <sivareddy.ecm@gmail.com>
2018-06-29 15:43:55 +0530

    SOAP Document Style Example

---
+++ SOAP-DOC/src/com/test/jaxws/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.test.jaxws.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import com.test.jaxws.beans.Person;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
+public interface PersonService {
+
+	@WebMethod
+	public boolean addPerson(Person p);
+	
+	@WebMethod
+	public boolean deletePerson(int id);
+	
+	@WebMethod
+	public Person getPerson(int id);
+	
+	@WebMethod
+	public Person[] getAllPersons();
+}
---
+++ SOAP-DOC/src/com/test/jaxws/service/SOAPPublisher.java
@@ -0,0 +1,13 @@
+package com.test.jaxws.service;
+
+import javax.xml.ws.Endpoint;
+
+public class SOAPPublisher {
+
+	public static void main(String[] args) {
+		System.out.println("Doc Starting...");
+		Endpoint.publish("http://localhost:8888/ws/person", new PersonServiceImpl());  
+		System.out.println("Closed");
+	}
+
+}
